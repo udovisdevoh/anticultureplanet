6fe9b551b6e38cab432426c9302e333085f004aa
glacasse <glacasse@981aca78-4533-8105-61b0-252b52ee4db5>
2010-07-17 21:05:27 +0000

    Added planet and planet generator stub

---
+++ trunk/models/Planet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AntiCulturePlanet
+{
+    /// <summary>
+    /// Represents a planet on which prehistoric people live
+    /// </summary>
+    internal class Planet
+    {
+        #region Fields
+        /// <summary>
+        /// Planet's width
+        /// </summary>
+        private int width;
+
+        /// <summary>
+        /// Planet's height
+        /// </summary>
+        private int height;
+        #endregion
+    }
+}
