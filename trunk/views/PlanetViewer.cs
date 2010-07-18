359b7fe1ad6d90a5a3a1d3ec9716d456c347f7ea
glacasse <glacasse@981aca78-4533-8105-61b0-252b52ee4db5>
2010-07-18 15:40:09 +0000

    It is now possible to scroll on the map

--- trunk/views/PlanetViewer.cs
+++ trunk/views/PlanetViewer.cs
@@ -10,10 +10,19 @@ namespace AntiCulturePlanet
     /// </summary>
     internal abstract class PlanetViewer
     {
+        #region Internal Methods
         /// <summary>
         /// Refresh planet display and mark as IsNeedRefresh = false
         /// </summary>
         /// <param name="planet">planet</param>
         internal abstract void Update(Planet planet);
+
+        /// <summary>
+        /// Move view
+        /// </summary>
+        /// <param name="tileOffsetX">horizontal tile offset</param>
+        /// <param name="tileOffsetY">vertical tile offset</param>
+        internal abstract void MoveView(int tileOffsetX, int tileOffsetY);
+        #endregion
     }
 }
