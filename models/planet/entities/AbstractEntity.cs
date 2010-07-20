31da599c63e9073033bcb5d2adbf1139d3377e17
glacasse <glacasse@981aca78-4533-8105-61b0-252b52ee4db5>
2010-07-20 01:30:38 +0000

    Added entity stub

---
+++ models/planet/entities/AbstractEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AntiCulturePlanet
+{
+    /// <summary>
+    /// Represents entities on the planet (people, trees, animals, rocks, tools)
+    /// </summary>
+    internal abstract class AbstractEntity
+    {
+    }
+}
