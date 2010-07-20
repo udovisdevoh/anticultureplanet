0ff3242431702b734148573b88986729eca5d2bd
glacasse <glacasse@981aca78-4533-8105-61b0-252b52ee4db5>
2010-07-20 12:47:49 +0000

    Added some properties to abstract entity

--- models/planet/entities/AbstractEntity.cs
+++ models/planet/entities/AbstractEntity.cs
@@ -10,5 +10,92 @@ namespace AntiCulturePlanet
     /// </summary>
     internal abstract class AbstractEntity
     {
+        #region Fields
+        /// <summary>
+        /// X position
+        /// </summary>
+        private double x;
+
+        /// <summary>
+        /// Y position
+        /// </summary>
+        private double y;
+
+        /// <summary>
+        /// Z position
+        /// </summary>
+        private double z;
+
+        /// <summary>
+        /// Mass
+        /// </summary>
+        private double mass;
+
+        /// <summary>
+        /// Size
+        /// </summary>
+        private double size;
+
+        /// <summary>
+        /// Decay Time (seconds)
+        /// </summary>
+        private double decayTime;
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// X position
+        /// </summary>
+        internal double X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        /// <summary>
+        /// X position
+        /// </summary>
+        internal double Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        /// <summary>
+        /// Z position
+        /// </summary>
+        internal double Z
+        {
+            get { return z; }
+            set { z = value; }
+        }
+
+        /// <summary>
+        /// Mass
+        /// </summary>
+        internal double Mass
+        {
+            get { return mass; }
+            set { mass = value; }
+        }
+
+        /// <summary>
+        /// Size
+        /// </summary>
+        internal double Size
+        {
+            get { return size; }
+            set { size = value; }
+        }
+
+        /// <summary>
+        /// Decay Time (seconds)
+        /// </summary>
+        internal double DecayTime
+        {
+            get { return decayTime; }
+            set { decayTime = value; }
+        }
+        #endregion
     }
 }
