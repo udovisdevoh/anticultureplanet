8dd2d90b2e4adb7281537e3027a26100de6729c6
glacasse <glacasse@981aca78-4533-8105-61b0-252b52ee4db5>
2010-07-21 00:18:34 +0000

    Added human classes stubs

---
+++ models/planet/entities/animals/humans/ManEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AntiCulturePlanet
+{
+    /// <summary>
+    /// Represents a man
+    /// </summary>
+    class ManEntity : AbstractHumanEntity
+    {
+    }
+}
---
+++ models/planet/entities/animals/humans/WomanEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AntiCulturePlanet
+{
+    /// <summary>
+    /// Represents a woman
+    /// </summary>
+    class WomanEntity : AbstractHumanEntity
+    {
+    }
+}
