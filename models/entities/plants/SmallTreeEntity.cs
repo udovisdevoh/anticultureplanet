1f0553041957c41c5696d8eb5161b57701961ca8
glacasse <glacasse@981aca78-4533-8105-61b0-252b52ee4db5>
2010-07-25 04:20:09 +0000

    Added trunks (small trees decay into trunks)

---
+++ models/entities/plants/SmallTreeEntity.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AntiCulturePlanet
+{
+    /// <summary>
+    /// Represents a small tree
+    /// </summary>
+    class SmallTreeEntity : AbstractEntity
+    {
+        protected override double BuildDecayTime()
+        {
+            return 50;
+        }
+
+        protected override double BuildSize()
+        {
+            return 1;
+        }
+
+        protected override double BuildMass()
+        {
+            return 0.25;
+        }
+
+        protected override EntitySprite BuildEntitySprite()
+        {
+            return SpriteManager.GetSprite(this.GetType());
+        }
+
+        protected override IEnumerable<AbstractEntity> GetDecayEntities(Planet planet, EntityCollection entityCollection)
+        {
+            return new AbstractEntity[] { new TrunkEntity() };
+        }
+
+        protected override PositionCriteria BuildPositionCriteria()
+        {
+            return PositionCriteria.Ground;
+        }
+
+        protected override ZIndexLayer BuildZIndexLayer()
+        {
+            return ZIndexLayer.OnFloor;
+        }
+
+        protected override bool BuildIsKeepSpriteOfPreviousEntity()
+        {
+            return false;
+        }
+
+        protected override bool BuildIsKeepMassOfPreviousEntity()
+        {
+            return false;
+        }
+
+        protected override bool BuildIsKeepSizeOfPreviousEntity()
+        {
+            return false;
+        }
+
+        protected override bool BuildIsAffectedByCollision()
+        {
+            return true;
+        }
+    }
+}
